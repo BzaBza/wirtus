195297312c64e90120418350b521270f4c34e8aa
bzabza <akkdlyapereliva@mail.ru>
2018-03-26 19:59:09 +0300

    add line chart first style

--- src/components/container/trending-charts/index.js
+++ src/components/container/trending-charts/index.js
@@ -1,33 +1,111 @@
 import React, {Component} from 'react';
-import LineChart from "../../dumb/line-chart/index";
-import lineChartData from '../../../redux/config/line-trending-chartdata-data';
+import MixChart from "../../dumb/mix-chart/index";
+import mixChartData from '../../../redux/config/mix-trending-chartdata-data';
 
 
 class TrendingCharts extends Component {
 
   render() {
 
+    const options = {
+      legend: false,
+      responsive: true,
+      tooltips: {
+        mode: 'label'
+      },
+      elements: {
+        line: {
+          fill: true,
+        }
+      },
+      scales: {
+        xAxes: [
+          {
+            id: 'x-axis-1',
+            display: true,
+            position: 'bottom',
+            showBorder:false,
+            gridLines: {
+              showBorder:false,
+              display: true
+            },
+            ticks: {
+              beginAtZero: false,
+              min: 0,
+              padding: -40,
+            },
+          },
+          {
+            id: 'x-axis-2',
+            display: true,
+            position: 'top',
+            showBorder:false,
+            gridLines: {
+              display: false,
+            },
+            scaleLabel: {
+              display: false
+            },
+            ticks: {
+              beginAtZero:true,
+              display: false
+            },
+          }
+        ],
+
+        yAxes: [
+          {
+            stacked: true,
+            type: 'linear',
+            display: true,
+            position: 'left',
+            id: 'y-axis-1',
+            gridLines: {
+              display: false,
+            },
+            scaleLabel: {
+              display: false
+            },
+            ticks: {
+              beginAtZero:true,
+              display: false
+            },
+            labels: {
+              show: false
+            }
+          },
+        ]
+      },
+    };
+
+
     let lineChartConfig = {
-      backgroundColor: '#294260',
-      borderColor: '#2196f3',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: '#ffffff',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: '#ffffff',
-      pointHoverBorderColor: '#256faf',
-      pointHoverBorderWidth: 3,
-      pointRadius: 1,
-      pointHitRadius: 1,
-      data: lineChartData.data
+      labels: ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'],
+      datasets: [{
+        xAxisID: 'x-axis-2',
+        type: 'line',
+        data: mixChartData.data,
+        fill: true,
+        borderColor: '#2196f3',
+        backgroundColor: 'rgba(41, 66, 97, 0.5)',
+        borderJoinStyle: 'miter',
+        pointBorderColor: '#ffffff',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: '#ffffff',
+        pointHoverBorderColor: '#256faf',
+        pointHoverBorderWidth: 3,
+        pointRadius: 1,
+        pointHitRadius: 1,
+      }, {
+        type: 'bar',
+        fill: false,
+      }],
     };
     return (
      <section className="col-12">
-        <LineChart lineChartConfig={lineChartConfig}/>
+       <MixChart lineChartConfig={lineChartConfig} options={options}/>
      </section>
     );
   }
---
+++ src/components/dumb/mix-chart/index.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {Bar} from 'react-chartjs-2';
+
+class MixChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lineChartConfig: {},
+      option: {}
+    };
+  }
+
+  componentWillMount() {
+
+    this.setState({
+      lineChartConfig: this.props.lineChartConfig,
+      option: this.props.options
+    })
+  }
+
+  render() {
+    return (
+     <Bar
+      data={this.state.lineChartConfig}
+      height={600}
+      width={1700}
+      options={this.state.option}
+     />
+    );
+  }
+}
+
+export default MixChart;
